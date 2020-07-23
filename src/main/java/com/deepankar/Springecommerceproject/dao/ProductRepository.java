198571c62634a60da4b0000d41c0c9c16bdcd6ac
Deepankar9106 <65331365+Deepankar9106@users.noreply.github.com>
2020-07-23 18:26:48 +0530

    First commit

---
+++ src/main/java/com/deepankar/Springecommerceproject/dao/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.deepankar.Springecommerceproject.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.deepankar.Springecommerceproject.entity.Product;
+
+
+@CrossOrigin("http://localhost:4200") 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
